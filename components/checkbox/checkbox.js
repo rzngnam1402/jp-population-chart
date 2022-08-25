ca2eda577d3ed049111948fe4e5cea84c3b91797
rzngnam1402 <legiangnam1402@gmail.com>
2022-08-25 15:33:22 +0700

    feat: add checkbox state

--- components/checkbox/checkbox.js
+++ components/checkbox/checkbox.js
@@ -1,8 +1,9 @@
+import { useState } from 'react';
 import useSWR from 'swr'
 import styles from './checkbox.module.css'
 
 const Checkbox = () => {
-
+    const [checked, setChecked] = useState([]);
     const { data, error } = useSWR('api/prefcode', (url) => fetch(url).then(res => res.json()));
     if (error) return <div>An error occured.</div>
     if (!data) return
@@ -12,14 +13,23 @@ const Checkbox = () => {
         </div>
     )
 
-    console.log(data);
+    const handleChange = (e) => {
+        if (e.target.checked === true) {
+            checked.push(e.target.id);
+        }
+        else {
+            checked = checked.filter(item => item !== e.target.id)
+        }
+        setChecked(checked)
+        console.log(checked);
+    }
 
     return (
         <div className={styles.container}>
             <div className={styles.boxes}>
                 {data.map(item => (
                     <div key={item.prefName} className={styles.box}>
-                        <input className={styles.input} type="checkbox" id={item.prefCode} />
+                        <input className={styles.input} type="checkbox" id={item.prefCode} onChange={e => handleChange(e)} />
                         <label htmlFor={item.prefCode} ><h2>{item.prefName}</h2></label>
                     </div>
                 ))}
